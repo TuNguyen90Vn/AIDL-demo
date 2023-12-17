90ea37f9dca7437c6e891d72f721dd509042be32
TN <tunguyen90vn@gmail.com>
2023-12-17 23:12:09 +0700

    First commit

---
+++ app/src/main/java/com/tony/myclient/BroadcastFragment.kt
@@ -0,0 +1,35 @@
+package com.tony.myclient
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tony.myclient.databinding.FragmentBroadcastBinding
+import com.tony.myserver.IIPCExample
+
+class BroadcastFragment : Fragment() {
+
+    private var _binding: FragmentBroadcastBinding? = null
+    private val binding get() = _binding!!
+    var iRemoteService: IIPCExample? = null
+    private var connected = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBroadcastBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
